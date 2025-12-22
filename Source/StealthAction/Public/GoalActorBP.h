12470b9c7c55f292c242085a8158f2ac3f13d167
momotasinon077 <momotasinon077@gmail.com>
2025-12-22 23:50:08 +0900

    ゴールのBPの追加と出現ポイントの設定

---
+++ Source/StealthAction/Public/GoalActorBP.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GoalActor.h"
+#include "GoalActorBP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTHACTION_API AGoalActorBP : public AGoalActor
+{
+	GENERATED_BODY()
+	
+};
