2e32fcf5684a893d4e63596786b31d0aec90731d
dai278 <daichi.hidaka@icloud.com>
2026-01-03 01:02:48 +0900

    aaa

---
+++ Source/StealthAction/Public/Checkpoint/Checkpoint.h
@@ -0,0 +1,36 @@
+// チェックポイント
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Checkpoint.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class STEALTHACTION_API ACheckpoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACheckpoint();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	//メッシュコンポーネント
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Checkpoint", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* m_pMesh;
+	//コリジョンコンポーネント
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Checkpoint", meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* m_pCollisionBox;
+
+};
